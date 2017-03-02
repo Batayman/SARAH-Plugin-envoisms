2adfe1c82f9174861c57cf6dedfb4f54bdf834a3
Batayman <jeromepereira@hotmail.com>
2017-03-02 11:09:12 +0100

    Add files via upload

---
+++ config/recupData.bat
@@ -0,0 +1,14 @@
+@echo off
+title Recuperation du User et des devices
+mode con cols=50 lines=20
+color 70
+
+:accessToken
+set /p token=Colle ton access-token : 
+if "%token%" equ "" ( 
+GOTO accessToken )
+
+
+curl --header "Access-Token: %token%" https://api.pushbullet.com/v2/users/me > user.json
+curl --header "Access-Token: %token%" https://api.pushbullet.com/v2/devices > devices.json
+pause
